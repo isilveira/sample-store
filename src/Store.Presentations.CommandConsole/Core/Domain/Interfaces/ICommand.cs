c5d920d7626fd46548c025d972bdacc5144c579f
Italo Silveira <italobrian@gmail.com>
2023-02-17 22:45:45 -0300

    console migrator and seeder

---
+++ src/Store.Presentations.CommandConsole/Core/Domain/Interfaces/ICommand.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.Presentations.CommandConsole.Interfaces
+{
+    interface ICommand
+    {
+        string Name { get; }
+        string CommandLine { get; }
+        void Run(out bool exit);
+    }
+}
